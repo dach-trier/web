dbef1dfd0488844c3594f711473f2a6f4e11ed43
Volodymyr But <git@hiegz.com>
2025-11-17 20:10:57 +0100

    create a new NextJS project

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import * as path from "path";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+    turbopack: {
+        root: path.join(__dirname, "."),
+    },
+};
+
+export default nextConfig;
