52c613cb6163dbaaf9eed7e50a2c7d818d412507
Volodymyr But <git@hiegz.com>
2025-11-20 19:29:07 +0100

    fix invalid locale id

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -1,7 +1,7 @@
 import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
-    locales: ["en", "de", "ua"],
+    locales: ["en", "de", "uk"],
     defaultLocale: "en",
     localePrefix: "never",
     localeCookie: true,
