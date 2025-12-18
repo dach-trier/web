391bfdd4342629e0a5627cd8a9b7d0f62d2884d9
Volodymyr But <git@hiegz.com>
2025-12-18 15:31:55 +0100

    add a checkmark icon

---
+++ icons/check.tsx
@@ -0,0 +1,31 @@
+type Props = {
+    width?: number | string;
+    height?: number | string;
+    stroke?: string;
+    strokeWidth?: number;
+};
+
+export default function Check({
+    width,
+    height,
+    stroke = "currentColor",
+    strokeWidth = 1.5,
+}: Props) {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke={stroke}
+            strokeWidth={strokeWidth}
+            width={width}
+            height={height}
+        >
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m4.5 12.75 6 6 9-13.5"
+            />
+        </svg>
+    );
+}
