16b718450ac410a06d64d337d2fd383e4ef2837d
Volodymyr But <git@hiegz.com>
2025-12-20 15:07:11 +0100

    add <Padding />

---
+++ ui/padding.tsx
@@ -0,0 +1,38 @@
+import React, { ReactNode } from "react";
+
+type Props = {
+    children: ReactNode;
+
+    x?: number | string;
+    left?: number | string;
+    right?: number | string;
+
+    y?: number | string;
+    top?: number | string;
+    bottom?: number | string;
+};
+
+export default function Padding({
+    x,
+    y,
+    top = y,
+    bottom = y,
+    left = x,
+    right = x,
+    children,
+}: Props) {
+    const child = React.Children.only(children);
+
+    return (
+        <div
+            style={{
+                paddingTop: top,
+                paddingBottom: bottom,
+                paddingLeft: left,
+                paddingRight: right,
+            }}
+        >
+            {child}
+        </div>
+    );
+}
