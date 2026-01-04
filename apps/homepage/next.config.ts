651538bb756ea62dcdbc723c7e05ca0c1a00320f
Volodymyr But <git@hiegz.com>
2026-01-04 02:58:51 +0100

    cleanup next.config.ts

--- apps/homepage/next.config.ts
+++ apps/homepage/next.config.ts
@@ -1,10 +1,5 @@
-import * as path from "path";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-    images: {
-        remotePatterns: [new URL("https://placehold.co/**")],
-    },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
