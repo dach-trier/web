75df200e885187805051e96dbbe530c178423e26
Volodymyr But <git@hiegz.com>
2026-03-06 10:19:26 +0100

    expand join us section to screen width

--- apps/homepage/app/components/join-us/join-us.tsx
+++ apps/homepage/app/components/join-us/join-us.tsx
@@ -4,24 +4,32 @@ import { ArrowRightIcon } from "@dach/svg";
 const JoinUs = () => {
     return (
         <div className={styles["join-us"]}>
-            <div className={styles["text"]}>
-                <h1>Join Us!</h1>
-                <p>Something inspirational</p>
-            </div>
+            <div className={styles["inner-container"]}>
+                <div className={styles["text"]}>
+                    <h1>Join Us!</h1>
+                    <p>Something inspirational</p>
+                </div>
 
-            <div className={styles["actions"]}>
-                <button className={styles["form"]}>
-                    Download Join Form
-                </button>
-
-                <div className={styles["examples"]}>
-                    <button>
-                        <ArrowRightIcon className={styles["arrow-right-icon"]}/> Example 1
+                <div className={styles["actions"]}>
+                    <button className={styles["form"]}>
+                        Download Join Form
                     </button>
 
-                    <button>
-                        <ArrowRightIcon className={styles["arrow-right-icon"]} /> Example 2
-                    </button>
+                    <div className={styles["examples"]}>
+                        <button>
+                            <ArrowRightIcon
+                                className={styles["arrow-right-icon"]}
+                            />{" "}
+                            Example 1
+                        </button>
+
+                        <button>
+                            <ArrowRightIcon
+                                className={styles["arrow-right-icon"]}
+                            />{" "}
+                            Example 2
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
